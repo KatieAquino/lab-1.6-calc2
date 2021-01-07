0724d8b9500baa3d5e6c8f5ff45b919e9c56f436
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-01-07 00:55:31 +0000

    Inital Commit

---
+++ calculator.py
@@ -0,0 +1,11 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+
+
+# Replace this with your code
+ocean_animals = 'shark squid tuna flounder'
+print(ocean_animals.split(','))
+print(ocean_animals.split(' '))
+# print(ocean_animals.split(''))
